c620246e297127851d74e68d340ebd264dc5915a
BuildTools <cephas8080@outlook.com>
2022-10-21 22:49:58 +0100

    Keyboard and basic game

--- src/Content.svelte
+++ src/Content.svelte
@@ -1,32 +1,79 @@
 <style>
     .center_content{
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
-        margin-top: 30%;
+        margin-top: 5%;
     }
     .ans{
-        width: calc(100vw / 10);
-        height: calc(100vw / 10);
+        flex-grow: 1;
+        width: calc(100vw / 20);
+        height: calc(100vh / 15);
+        text-align: center;
+    }
+    .textans{
+        display: flex;
+    }
+    .space{
+        border: grey solid 5px;
+    }
+    @media (max-width: 600px){
+        .ans{
+            width: calc(100vw / 10);
+            height: calc(100vh / 10);
+        }
     }
 </style>
+
 <script>
     import {onMount} from 'svelte';
     import Loading from './Loading.svelte'
+    import Keyboard from './Keyboard.svelte'
+    function OnKeyPress_Keyboard(event){
+        if (ans.toLowerCase().indexOf(event.detail.key.toLowerCase()) === -1){
+            time_usergo_wrong += 1
+        }else{
+            time_usergo_right += 1
+        }
+    }
     let finishfetch = false
+    let time_usergo_wrong = 0
+    let time_usergo_right = 0
+    let userspace
+    let ans 
+    let cover_imageurl
+    let regex = new RegExp(/[^a-z,A-Z]/)
     onMount(async ()=>{
         let random_num = Math.floor(Math.random()*100)
         let content = await fetch("https://igdb-gatewayexpress.yeungcephas.repl.co/gamelist/6/90/"+random_num)
         let output = await content.json()
         finishfetch = true
-        console.log(output)
+        userspace = output[random_num - 1]
+        console.log(userspace)
+        ans = userspace["name"]
+    // debug tag unuse soon
+    console.log(ans)
     })
 </script>
-{#if finishfetch === false}
+<div class="center_content">
+{#if finishfetch !== false}
+    {#each ans.split(" ") as x,i}
+        <div class="textans">
+        {#each x.split("") as y,u}
+            {#if regex.test(y) === true}
+            <!-- check if(the text is not a to z or not) and then it will do this operation -->
+            <input class="ans space" type="text" maxlength="1" disabled value={y} id="{i}:{u}">
+            {:else}
+            <input class="ans" type="text" maxlength="1" disabled id="{i}:{u}">
+            {/if}
+        {/each}
+        </div>
+        <br>
+    {/each}
+{:else}
     <Loading />
 {/if}
-<div class="center_content">
-    <input class="ans" type="text" maxlength="1">
+<h1>input the text to start guessing:</h1>
+<Keyboard on:keypress={OnKeyPress_Keyboard} ans={ans}/>
 </div>
-
-
---
+++ src/Keyboard.svelte
@@ -0,0 +1,84 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+    export let ans = "";
+    let correct_pressed_array = []
+    let incorrect_pressed_array = []
+    const dispatch = createEventDispatcher();
+    function onClickKey(keypressed){
+        if(ans.toLowerCase().indexOf(keypressed.toLowerCase()) !== -1){
+            if(correct_pressed_array.indexOf(keypressed) === -1){
+                correct_pressed_array.push(keypressed)
+                let query_selection = document.querySelector(`[data-key="${keypressed}"]`)
+                query_selection.style.backgroundColor = "green";
+            }
+        }else{
+            let query_selection = document.querySelector(`[data-key="${keypressed}"]`)
+            query_selection.style.backgroundColor = "red";
+        }
+        dispatch('keypress',{"key":keypressed})
+    }
+    
+</script>
+<style>
+    .keyboardbutton{
+        width: calc(100vw / 25);
+        height: calc(100vh / 15);
+        background-color: grey;
+        border: grey;
+        border-radius: 5px;
+        margin: 3px 3px 3px 3px;
+    }
+    .row{
+        display: flex;
+        flex-direction: row;
+    }
+    .col{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    @media (max-width: 600px){
+        .keyboardbutton{
+            width: calc(100vw / 13);
+            height: calc(100vh / 15);
+        }
+    }
+    .keyboardbutton:active{
+        background-color: gainsboro;
+    }
+</style>
+<div class="col">
+    <div class="row">
+        <button class="keyboardbutton" on:click={() => onClickKey("Q")} data-key="Q">Q</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("W")} data-key="W">W</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("E")} data-key="E">E</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("R")} data-key="R">R</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("T")} data-key="T">T</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("Y")} data-key="Y">Y</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("U")} data-key="U">U</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("I")} data-key="I">I</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("O")} data-key="O">O</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("P")} data-key="P">P</button>
+    </div>
+    <div class="row">
+        <button class="keyboardbutton" on:click={() => onClickKey("A")} data-key="A">A</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("S")} data-key="S">S</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("D")} data-key="D">D</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("F")} data-key="F">F</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("G")} data-key="G">G</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("H")} data-key="H">H</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("J")} data-key="J">J</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("K")} data-key="K">K</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("L")} data-key="L">L</button>
+    </div>
+    <div class="row">
+        <button class="keyboardbutton" on:click={() => onClickKey("Z")} data-key="Z">Z</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("X")} data-key="X">X</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("C")} data-key="C">C</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("V")} data-key="V">V</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("B")} data-key="B">B</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("N")} data-key="N">N</button>
+        <button class="keyboardbutton" on:click={() => onClickKey("M")} data-key="M">M</button>
+    </div>
+</div>
